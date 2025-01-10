3bafa72e0bfd8eed636d59b1bbb25e39a0b72779
zefanad <nadhirahauraramadhani@gmail.com>
2025-01-10 21:01:48 +0700

    tambah gambar

--- src/components/front-end/Banner.tsx
+++ src/components/front-end/Banner.tsx
@@ -4,7 +4,7 @@ const Banner = () => {
             <div className="grid grid-cols-1 lg:grid-cols-[66%,34%] gap-6 lg:gap-8 pr-4 lg:pr-0">
                 {/* Gabungan Left dan Right Banner */}
                 <div 
-                    className="h-[300px] md:h-[400px] lg:h-[500px] bg-[url('/path/to/your/combined-banner.jpg')] bg-cover bg-center rounded-xl p-6 md:p-10 lg:p-12 flex flex-col justify-between shadow-md hover:shadow-lg transition-shadow"
+                    className="h-[300px] md:h-[400px] lg:h-[500px] bg-[url('/products/header.jpg')] bg-cover bg-center rounded-xl p-6 md:p-10 lg:p-12 flex flex-col justify-between shadow-md hover:shadow-lg transition-shadow"
                 >
                     <div>
                         <p className="text-blue-400 text-lg md:text-xl font-medium">
